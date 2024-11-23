0c7d92c5207102cad0cd6f8d22da82fb766aafe3
mvargas33 <mvargas.uchile@gmail.com>
2024-11-23 17:16:32 -0300

    :hammer: Fix solver

--- src/routes/exercise-router.ts
+++ src/routes/exercise-router.ts
@@ -14,6 +14,46 @@ exerciseRouter.get('/all', async (req, res) => {
   res.status(200).json({ message: exercises});
 });
 
+
+exerciseRouter.post('/solve', async (req, res) => {
+  const { identifier } = req.body;
+  console.log('Solving exercise:', identifier);
+  const db = await getMongoConnection('exercises');
+  console.log('AA')
+  const exercise = await get(db, 'exercises', { identifier });
+
+  console.log('Exercise found:', exercise);
+
+  if (!exercise) {
+    res.status(404).json({ message: 'Exercise not found' });
+    return;
+  }
+
+  if (exercise.explanation) {
+    res.status(400).json({ message: 'Exercise already solved', exercise });
+    return;
+  }
+
+  console.log('Solving exercise ...');
+
+  const solverPrompt = solveExercisePrompt(exercise);
+  const explanation = await askChatGPT(solverPrompt);
+
+  console.log('Explanation:', explanation);
+
+  const newExercise = {
+    ...exercise,
+    explanation,
+  };
+
+  const result = await upsert(db, 'exercises', newExercise, { identifier });
+  const final = await get(db, 'exercises', { identifier });
+
+  console.log('Exercise solved:', final);
+
+  res.status(200).json({ message: 'ok', result, exercise: final });
+});
+
 exerciseRouter.get('/:identifier', async (req, res) => {
   const { identifier } = req.params;
 
@@ -34,36 +74,6 @@ exerciseRouter.post('/:identifier?', async (req, res) => {
   const uspertedExercise = await upsert(db, 'exercises', body, { identifier });
 
   res.status(201).json({ message: uspertedExercise });
-});
-
-exerciseRouter.post('/solve', async (req, res) => {
-  const { identifier } = req.body;
-
-  const db = await getMongoConnection('exercises');
-  const exercise = await get(db, 'exercises', { identifier });
-
-  if (!exercise) {
-    res.status(404).json({ message: 'Exercise not found' });
-    return;
-  }
-
-  if (exercise.explanation) {
-    res.status(400).json({ message: 'Exercise already solved', exercise });
-    return;
-  }
-
-  const solverPrompt = solveExercisePrompt(exercise);
-  const explanation = await askChatGPT(solverPrompt);
-
-  const newExercise = {
-    ...exercise,
-    explanation,
-  };
-
-  const result = await upsert(db, 'exercises', newExercise, { identifier });
-  const final = await get(db, 'exercises', { identifier });
-
-  res.status(200).json({ message: 'ok', result, exercise: final });
 });
 
 export default exerciseRouter;
