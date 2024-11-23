2f1309632fe3e5c1f3e6c519595d4d1c282ceb1e
ValentinaGonzalezF <v.gonzalez.2@ug.uchile.cl>
2024-11-23 19:50:21 -0300

    Add server config

---
+++ server/server.tsx
@@ -0,0 +1,42 @@
+//import Config from 'react-native-config'
+const serverIP = '';
+
+const get = async (url: string) => {
+  try {
+    const response = await fetch(`${serverIP}${url}`, {
+      method: 'GET',
+      mode: 'no-cors',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+    });
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const post = async (url: string, body: Record<string, unknown>) => {
+  try {
+    const response = await fetch(`${serverIP}${url}`, {
+      method: 'POST',
+      mode: 'no-cors',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export default {
+  post,
+  get,
+};
--- server/websocket.tsx
+++ server/websocket.tsx
@@ -1,24 +1,28 @@
 import { SetStateAction, useEffect } from 'react';
 import useWebSocket, { ReadyState } from 'react-use-websocket';
-import {MessageInput} from '../components/ChatMessage'
+import { MessageInput } from '../components/ChatMessage';
 
-function WebsocketTerminal ({setMessages}: {setMessages: React.Dispatch<SetStateAction<MessageInput[]>>}) {
+function WebsocketTerminal({
+  setMessages,
+}: {
+  setMessages: React.Dispatch<SetStateAction<MessageInput[]>>;
+}) {
   const ipServer = 'ws://192.168.1.52:8765';
-  const { sendJsonMessage, lastMessage, lastJsonMessage, readyState } = useWebSocket(ipServer);
+  const { sendJsonMessage, lastMessage, lastJsonMessage, readyState } =
+    useWebSocket(ipServer);
 
   useEffect(() => {
     if (lastMessage !== null) {
-    setMessages((prev) => [
-      ...prev,
-      { text: lastMessage as unknown as string, origin: 'bot' },
-    ]);
+      setMessages((prev) => [
+        ...prev,
+        { text: lastMessage as unknown as string, origin: 'bot' },
+      ]);
     }
     if (lastJsonMessage !== null) {
       console.log(JSON.stringify(lastJsonMessage));
     }
-    return () => {
-    };
-  }, [lastMessage, setMessages,lastJsonMessage]);
+    return () => {};
+  }, [lastMessage, setMessages, lastJsonMessage]);
 
   const connectionStatus = {
     [ReadyState.CONNECTING]: 'Connecting',
@@ -28,21 +32,27 @@ function WebsocketTerminal ({setMessages}: {setMessages: React.Dispatch<SetState
     [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
   }[readyState];
 
-  const sendMessageToSocket = ({message, question}:{message:string, question:string}) => {
+  const sendMessageToSocket = ({
+    message,
+    question,
+  }: {
+    message: string;
+    question: string;
+  }) => {
     const object = {
       type: 'setParam',
       message,
       question,
-    }
+    };
     sendJsonMessage(object);
-  }
-  
-return {
-  sendMessageToSocket,
-  readyState,
-  lastMessage,
-  lastJsonMessage
-}
+  };
+
+  return {
+    sendMessageToSocket,
+    readyState,
+    lastMessage,
+    lastJsonMessage,
+  };
 }
 
 export default WebsocketTerminal;
