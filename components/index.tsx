326a54a7c70c2fea38a3a87d769f21e8a2c3b85a
gabrielaelisa <30665865+gabrielaelisa@users.noreply.github.com>
2024-11-24 01:38:41 -0300

    fix: imports

--- components/index.tsx
+++ components/index.tsx
@@ -2,5 +2,5 @@ import { ProgressBar, size } from './ui/ProgressBar';
 import AlternativeSelection from './Alternative';
 import { MyButton } from './Button';
 import AssignmentCard from './AssignmentCard';
-import Units from './Units';
+import Units from './units';
 export { ProgressBar, AlternativeSelection, MyButton, AssignmentCard, size, Units };
